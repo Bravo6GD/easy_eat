f7bece449adea617fbf72d1265328f02ac303b07
Bravo6GD <messiashraya@gmail.com>
2022-04-18 10:47:16 +0545

    first commit

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+$server = "localhost";
+$username = "root";
+$password = "";
+$database = "easy_eat";
+$conn = mysqli_connect($server, $username, $password,);
+
+
+if($conn == false){
+    die('Error: Cannot connect');
+}
+
+?>
+?>
+
+
